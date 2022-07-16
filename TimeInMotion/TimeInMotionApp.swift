807e0ace2580b3a2e2ecb717eb64453c9a14e1f7
Ben Howlett <ben@benhowlett.com>
2022-07-16 09:05:52 -0600

    Initial Commit

---
+++ TimeInMotion/TimeInMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeInMotionApp.swift
+//  TimeInMotion
+//
+//  Created by Ben Howlett on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct TimeInMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
